97140171870205d92f8cc3d4d158718054c8ee48
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-03-06 22:39:25 +0800

    Create x571gt.nix

---
+++ x571gt.nix
@@ -0,0 +1,74 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+  system.stateVersion = "23.11";
+
+  boot.kernelParams = [
+    "mitigations=off"
+  ];
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    brave
+    ffmpeg
+    idevicerestore
+    libimobiledevice
+    unzip
+    virt-manager
+    wget
+    yt-dlp
+  ];
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-emoji
+  ];
+
+  hardware.opengl.extraPackages = with pkgs; [
+    intel-media-driver
+  ];
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  networking.firewall.enable = false;
+  networking.hostName = "X571GT";
+  networking.networkmanager.wifi.powersave = false;
+  networking.timeServers = [
+    "time.cloudflare.com"
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  security.rtkit.enable = true;
+
+  services.flatpak.enable = true;
+  services.printing.enable = true;
+  services.usbmuxd.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  #services.xserver.videoDrivers = [
+  #  "nvidia"
+  #];
+  services.xserver.xkbVariant = "colemak_dh";
+
+  time.timeZone = "Asia/Manila";
+
+  users.users.la.extraGroups = [
+    "libvirtd"
+    "networkmanager"
+    "wheel"
+  ];
+  users.users.la.isNormalUser = true;
+  users.users.la.initialPassword = "nixos";
+
+  virtualisation.libvirtd.enable = true;
+
+  zramSwap.enable = true;
+}
